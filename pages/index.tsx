900fead46034fa8c4adfdbca0c77bf7ccf3c4911
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-08-17 18:07:10 +0200

    working pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,36 +2,38 @@ import React from "react";
 import MainLayout from "../layouts/main.layout";
 import Posts from "../components/post/Posts.component";
 import Pagination from "../components/post/Pagination.component";
+import Loader from "../components/ui/Loader.component";
 import { IPosts } from "../models/response/posts.interface";
 import { useGetPostsService as UseGetPostsService } from "../services/post/useGetPosts.service";
 import { GetServerSideProps } from "next";
 
 const Home = ({ posts }: { posts: IPosts }) => {
+  const [loading, setLoading] = React.useState(false);
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   const { getPosts } = UseGetPostsService();
 
-  const changeRowsPerPage = (rows: number) => {
+  const changeRowsPerPage = async (rows: number) => {
     setRowsPerPage(rows)
-    fetchPosts().then()
+    await fetchPosts(rowsPerPage * page, rowsPerPage)
   }
 
-  const changePage = (page: number) => {
+  const changePage = async (page: number) => {
     setPage(page)
-    fetchPosts().then()
+    await fetchPosts(rowsPerPage * page, rowsPerPage)
   }
 
-  const fetchPosts = async () => {
-    posts = await getPosts({
-      offset: 0,
-      limit: rowsPerPage
-    })
+  const fetchPosts = async (offset: number, limit: number) => {
+    setLoading(true)
+    posts = await getPosts({ offset, limit })
+    setLoading(false)
   }
 
   return (
     <MainLayout>
       <>
+        {loading ? (<Loader/>) : null}
         <h1 className={'mt-3 text-center text-3xl font-extrabold text-gray-900'}>
           {posts.rows.length ? (<>List of posts</>) : (<>No posts yet</>)}
         </h1>
