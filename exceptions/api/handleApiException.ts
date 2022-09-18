d04ae6504432be9cada1b6ea20598cf2f81defcb
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-09-18 15:18:38 +0200

    fixes

--- exceptions/api/handleApiException.ts
+++ exceptions/api/handleApiException.ts
@@ -1,15 +1,15 @@
-import axios, { AxiosError } from "axios";
+import axios  from "axios";
 import { ApiException } from "@exceptions/ApiException.exception";
-import { GeneralException } from "@exceptions/GeneralException.exception";
 
 export const handleApiException = (error: Error) => {
-  // if (axios.isAxiosError(error)) {
-  //   console.log(error)
-  //   throw new ApiException(
-  //     <AxiosError>error.response.data.message.message,
-  //     error.response.data.message.message,
-  //     error.response.data.message.error,
-  //   );
-  // }
-  throw new GeneralException(error.message);
+  if (axios.isAxiosError(error)) {
+    throw new ApiException(
+      // @ts-ignore
+      error.response.data.message.message,
+      // @ts-ignore
+      error.response.data.message.message,
+      // @ts-ignore
+      error.response.data.message.error,
+    );
+  }
 };
--- layouts/auth.layout.tsx
+++ layouts/auth.layout.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import AuthHeader from "@components/ui/AuthHeader.component";
+import { useRouter } from "next/router";
 
 const AuthLayout = ({ children }: { children: JSX.Element }) => {
+  const router = useRouter();
+
   return (
     <>
-      <AuthHeader signInRoute={true} />
+      <AuthHeader signInRoute={router.pathname === '/sign-up'} />
       {children}
     </>
   );
