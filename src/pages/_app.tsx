840eeae2c5f7d7eb7d8369e8091e5cf32efabcc2
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-02-10 12:20:10 +0000

    ?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import '@styles/globals.scss';
 import React from 'react';
 
-import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { appWithTranslation } from 'next-i18next';
 import { RecoilRoot } from 'recoil';
 
 const App = ({ Component, pageProps }: AppProps) => {
