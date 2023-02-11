2be18ff0608c8a0ceda2ea446e4a230a1e60884a
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-02-11 19:46:10 +0100

    mobile part

--- src/styles/pages/post.style.ts
+++ src/styles/pages/post.style.ts
@@ -5,23 +5,43 @@ export const ArticleTitle = styled.h1`
   font-size: 72px;
   text-align: center;
   &.intro {
-    font-size: 22px;
-    font-weight: 400;
+    font-size: 24px;
+    font-weight: 100;
     margin: 75px 0;
+
+    @media only screen and (max-width: 780px) {
+      font-size: 20px;
+      margin: 25px 0;
+    }
+  }
+
+  @media only screen and (max-width: 780px) {
+    font-size: 48px;
   }
 `;
 
 export const ArticleBodyWrapper = styled.div`
   width: 45%;
   margin: 100px auto 0 auto;
+  @media only screen and (max-width: 780px) {
+    width: 90%;
+  }
 `;
 
 export const PostParagraph = styled.p`
+  font-family: 'Charter', sans-serif;
   color: rgb(${(props) => props.theme.colors.textColor});
-  font-size: 18px;
-  padding-top: 15px;
-  font-weight: 100;
+  font-size: 20px;
+  line-height: 30px;
+  padding-top: 30px;
+  text-indent: 50px;
+  font-weight: 400;
   &:last-child {
     margin-bottom: 200px;
+  }
+
+  @media only screen and (max-width: 780px) {
+    font-size: 18px;
+    text-indent: 25px;
   }
 `;
