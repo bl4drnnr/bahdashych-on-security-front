51f67f67a90b1270577056cdffa238dd84e1b19b
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-02-13 12:58:18 +0100

    new post

---
+++ src/pages/[locale]/blog/nextjs-nginx-deployment.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
+
+import DefaultLayout from '@layouts/Default.layout';
+import { getStaticPaths, makeStaticProps } from '@lib/getStatic';
+
+interface NextjsNginxDeploymentProps {
+  locale: string;
+}
+
+const NextjsNginxDeployment = ({ locale }: NextjsNginxDeploymentProps) => {
+  const { t } = useTranslation();
+  const router = useRouter();
+
+  const handleRedirect = async (path: string) => {
+    await router.push(`/${locale}${path}`);
+  };
+
+  return (
+    <>
+      <DefaultLayout locale={locale} translation={t}>
+        <></>
+      </DefaultLayout>
+    </>
+  );
+};
+
+const getStaticProps = makeStaticProps(['pages', 'components', 'common', 'articles']);
+export { getStaticPaths, getStaticProps };
+
+export default NextjsNginxDeployment;
--- src/pages/[locale]/projects/index.tsx
+++ src/pages/[locale]/projects/index.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import { useTranslation } from 'next-i18next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import Typewriter from 'typewriter-effect';
 
 import DefaultLayout from '@layouts/Default.layout';
 import { getStaticPaths, makeStaticProps } from '@lib/getStatic';
+import { ProjectsDescription, ProjectsWrapper, ProjectTitle } from '@styles/projects.style';
 
 
 interface ProjectsProps {
@@ -26,7 +28,21 @@ const Projects = ({ locale }: ProjectsProps) => {
         <title>{t('pages:home.name')} | {t('pages:projects.title')}</title>
       </Head>
       <DefaultLayout locale={locale} translation={t}>
-        <></>
+        <ProjectsWrapper>
+          <ProjectTitle>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .changeDelay(75)
+                  .typeString(t('pages:projects.description'))
+                  .start();
+              }}
+            />
+          </ProjectTitle>
+          <ProjectsDescription className={'margins'}>
+            {t('pages:projects.description2')}
+          </ProjectsDescription>
+        </ProjectsWrapper>
       </DefaultLayout>
     </>
   );
