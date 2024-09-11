5cd91274b38d6454ae0cb0ecf7834016fe48affb
bl4drnnr <mikhail.bahdashych@protonmail.com>
2024-09-11 17:51:27 +0200

    i18n for ssr

--- src/app/components/layout-components/layout-components.module.ts
+++ src/app/components/layout-components/layout-components.module.ts
@@ -2,19 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { GlobalMessageComponent } from '@components/global-message/global-message.component';
 import { BasicComponentsModule } from '@components/basic-components.module';
-import { TranslocoModule } from '@ngneat/transloco';
 import { LoaderComponent } from '@components/loader/loader.component';
 
 const components = [GlobalMessageComponent, LoaderComponent];
 
 @NgModule({
   declarations: [...components],
-  imports: [
-    CommonModule,
-    NgOptimizedImage,
-    BasicComponentsModule,
-    TranslocoModule
-  ],
+  imports: [CommonModule, NgOptimizedImage, BasicComponentsModule],
   exports: [...components]
 })
 export class LayoutComponentsModule {}
