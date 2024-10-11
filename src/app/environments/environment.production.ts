e4f0b566bba7c5a0967db74bc78f016fe756f973
bl4drnnr <mikhail.bahdashych@protonmail.com>
2024-10-11 20:56:04 +0200

    test commit to test cicd pipeline

--- src/app/environments/environment.production.ts
+++ src/app/environments/environment.production.ts
@@ -2,6 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'http://localhost:4201/api',
   baseUrl: 'http://localhost:4202',
-  staticStorage:
-    'https://mikhail-bahdashych-personal-blog.s3.us-east-2.amazonaws.com'
+  staticStorage: 'https://bahdashych-on-security.s3.eu-central-1.amazonaws.com'
 };
